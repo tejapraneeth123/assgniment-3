20331c20b3d3b3242c829a6ac909618da86f1e9b
tejapraneeth123 <btejapraneeth606@gmail.com>
2026-03-18 00:03:13 -0700

    task2.py

---
+++ task2.py
@@ -0,0 +1,11 @@
+import math
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+logarithm = math.log(num)
+sine_value = math.sin(num)
+
+
+print("Square root:", square_root)
+print("Logarithm:", logarithm)
+print("Sine:", sine_value)
